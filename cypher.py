74b4f30dc492ed8eec310c0e99d66aadfc7a652a
Gus Mendez <gus.mendez.99@gmail.com>
2020-08-06 19:50:10 -0600

    decode fixed

--- cypher.py
+++ cypher.py
@@ -7,11 +7,11 @@ secret = bytes('CC3078', 'utf-8')
 
 signature = base64.b64encode(hmac.new(secret, message, digestmod=hashlib.sha256).digest())
 print("El primer mensaje: ") 
-print(signature)
+print(base64.b64decode(signature).hex())
 
-message = bytes('“La implementacion de este ejercicio fue sencilla', 'utf-8')
+message = bytes('La implementacion de este ejercicio fue sencilla', 'utf-8')
 secret = bytes('MAC', 'utf-8')
 
 signature = base64.b64encode(hmac.new(secret, message, digestmod=hashlib.sha256).digest())
 print("El segundo mensaje: ")
-print(signature)
+print(base64.b64decode(signature).hex())
